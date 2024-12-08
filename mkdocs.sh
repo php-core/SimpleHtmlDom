031a1dd4e174862181e65ce4566b3801e2ea715e
php-core <dev@php-core.com>
2024-12-09 02:39:05 +0300

    Update docs

--- mkdocs.sh
+++ mkdocs.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-if [ ! -f venv ]; then
+if [ ! -d venv ]; then
   python3 -m venv venv
 fi
 
@@ -9,4 +9,9 @@ python3 -m pip install --upgrade pip
 python3 -m pip install mkdocs mkdocs-redirects
 
 mkdocs build
+
+if [ -d docs-html ]; then
+  rm -rf docs-html
+fi
+
 mv site docs-html
